a92c8544b4fae2337ce2f23d4dd217d89b436fc6
SZun <zunsamuel97@gmail.com>
2022-10-27 14:59:20 -0500

    updated install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
+jupyter kernelspec remove workingtitleenv
+conda env remove -n workingtitleenv
 conda env create -f environment.yml
 jupyter kernelspec remove dev
-conda activate workingtitleenv
 conda install -c conda-forge nb_conda_kernels -y
-conda deactivate
+jupyter lab
